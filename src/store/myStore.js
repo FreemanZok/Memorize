31e6bb5e6934ae75faeed433574e5d8c761f0a57
ali zokaei <ali.zokaei.1367@gmail.com>
2023-06-29 21:37:36 +0330

    save my high project

--- src/store/myStore.js
+++ src/store/myStore.js
@@ -1,5 +1,54 @@
-import { reactive } from 'vue'
+import { reactive } from "vue";
 
 export const myStore = reactive({
-  alizoka: "علی ذکایی"
-})
+  MainList: [
+    {
+      word: "erneut",
+      text_fa: "دوباره",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "how-to-say-again-in-japanese.jpg",
+    },
+    {
+      word: "hochladen",
+      text_fa: "آپلود",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "",
+    },
+    {
+      word: "herunterladen",
+      text_fa: "دانلود",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "",
+    },
+    {
+      word: "Dorf",
+      text_fa: "روستا",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "",
+    },
+    {
+      word: "Freunde",
+      text_fa: "دوستان",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "",
+    },
+    {
+      word: "miteinander",
+      text_fa: "به اشتراک",
+      // text_en:"anew",
+      // voice_en:"دوباره",
+      voice_fa: "",
+      picture: "",
+    },
+  ],
+});
